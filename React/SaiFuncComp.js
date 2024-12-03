f145780b8b297da4392d96aca4773a8386d489f2
Sai Kutthalingam S <saiakash200236@gmail.com>
2024-12-03 18:01:44 +0530

    i add day6 tasks

---
+++ React/SaiFuncComp.js
@@ -0,0 +1,11 @@
+const NameFunctionComp=(props)=>{
+    return (
+        <div>
+            <h2>Sai Kutthalingam S</h2>
+            <p>My Fullname is {props.fname} {props.lname}.<br></br>My salary is {props.salary}.<br></br> My gender is {props.gender}.<br></br> My address is {props.address}</p>
+        </div>
+
+    )
+};
+
+export default NameFunctionComp; 
